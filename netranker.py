28959624a57767342143927574383232c552c9f2
Daniel Strong <dstrong@glyx.co.uk>
2018-12-15 16:51:08 +0000

    Add basic test for CardSampler port

--- netranker.py
+++ netranker.py
@@ -1,6 +1,7 @@
 from uuid import uuid4 as uuid
 
 class Pairing():
+
     def __init__(self):
         self.uuid = uuid()
         self.cards = [0,0]
--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 import unittest
 
-from netranker import Pairing
+from netranker.core import Pairing
+
+from netranker.ports import CardSampler
 
 class TestPairing(unittest.TestCase):
 
     def test_init(self):
+        '''
+        Test that Pairings initialise correctly with a UUID and a pair of cards
+        '''
         pairing = Pairing()
 
         self.assertTrue(hasattr(pairing, 'uuid'))
@@ -13,8 +18,24 @@ class TestPairing(unittest.TestCase):
         self.assertEqual(len(pairing.cards), 2)
 
     def test_uniqueness(self):
+        '''
+        Test that the UUIDs on two different Pairings are unique, and that each
+        Pairing is of two unique cards.
+        '''
         pair1 = Pairing()
         pair2 = Pairing()
 
         self.assertNotEqual(pair1.uuid, pair2.uuid)
         self.assertNotEqual(pair1.uuid, pair2.uuid)
+
+        # need to implement card fetch port
+        # self.assertNotEqual(pair1.cards[0], pair1.cards[1])
+        # self.assertNotEqual(pair2.cards[0], pair2.cards[1])
+
+class CardSampler(unittest.TestCase):
+
+    def test_error_with_no_adapter():
+        '''
+        The port should fail to initialise with no adapter
+        '''
+        self.assertRaises(CardSampler(), TypeError)
