28f4cc382349a2349ed03f92c6f9a04c2d156648
Daniel Strong <dstrong@glyx.co.uk>
2019-05-20 23:51:02 +0100

    Add JWT tokens to pairings as identifiers

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, jsonify
 
+from datetime import datetime, timedelta
+
+import jwt
+
 app = Flask(__name__)
 
 @app.route("/pairing", methods=['GET'])
 def pairing():
-    response_json = {
-        'pairing': ['Temujin Contract', 'Şifr'],
-        'token': 'foo'
+    pairing = ['Temujin Contract', 'Şifr']
+
+    issued = datetime.utcnow()
+    jwt_claim = {
+        'pair': pairing,
+        'iat': issued,
+        'exp': issued + timedelta(hours=12)
     }
+
+    response_json = {
+        'pairing': pairing,
+        'token': jwt.encode(jwt_claim, app.config['signing_key'], algorithm='HS256').decode('utf-8') }
 
     return jsonify(response_json)
--- functional_tests.py
+++ functional_tests.py
@@ -7,6 +7,8 @@ from app import app
 class TestVoting(unittest.TestCase):
 
     def setUp(self):
+        self.signing_key = 'test signing key'
+        app.config['signing_key'] = self.signing_key
         self.client = app.test_client()
 
     def test_get_new_pairing(self):
@@ -20,3 +22,7 @@ class TestVoting(unittest.TestCase):
         self.assertEqual(len(pairing), 2)
 
         token = response.json.get('token', None)
+        try:
+            jwt.decode(token, self.signing_key, algorithms=['HS256'])
+        except jwt.DecodeError as exception:
+            self.fail('Could not decode pairing jwt: %s' % exception)
