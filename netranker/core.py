2db49f4c93627a175b761ed8a12480ef7e0e41e1
Daniel Strong <dstrong@glyx.co.uk>
2019-05-20 20:55:08 +0100

    Reset repo to empty repo

--- netranker/core.py
+++
@@ -1,7 +0,0 @@
-from uuid import uuid4 as uuid
-
-class Pairing():
-
-    def __init__(self, card_sampler):
-        self.uuid = uuid()
-        self.cards = card_sampler.sample()
--- test/adapters.py
+++
@@ -1,9 +0,0 @@
-from netranker.ports import CardSampler
-
-class TestSampler(CardSampler):
-
-    def __init__(self, mock_sample):
-        self._mock_sample = mock_sample
-
-    def sample(self):
-        return self._mock_sample
