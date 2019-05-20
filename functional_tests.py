44631a4f9c489b412c2f2d99eda0a7273e37ca15
Daniel Strong <dstrong@glyx.co.uk>
2019-05-20 23:02:49 +0100

    Use flask test client instead of requests

--- functional_tests.py
+++ functional_tests.py
@@ -1,18 +1,22 @@
 import unittest
 
 import jwt
-import requests
+
+from app import app
 
 class TestVoting(unittest.TestCase):
 
+    def setUp(self):
+        self.client = app.test_client()
+
     def test_get_new_pairing(self):
-        response = requests.get('http://localhost:5000/pairing')
+        response = self.client.get('/pairing')
 
         self.assertEqual(response.status_code, 200)
 
         self.assertEqual(response.headers['content-type'], 'application/json')
 
-        pairing = response.json().get('pairing', None)
+        pairing = response.json.get('pairing', None)
         self.assertEqual(len(pairing), 2)
 
-        token = response.json().get('token', None)
+        token = response.json.get('token', None)
