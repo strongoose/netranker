1ff895916bb9578af9c857be1c6ce63cc84a5a79
Daniel Strong <dstrong@glyx.co.uk>
2019-05-22 16:55:26 +0100

    Load card and set data from netrunnerdb API

--- utils.py
+++ utils.py
@@ -1,4 +1,18 @@
 import requests
+from itertools import groupby
+
+def get_title(card):
+    return card['title']
 
 def load_cards(collection):
-    collection.insert_one({'name': 'Corroder'})
+    card_data = requests.get(
+        'https://netrunnerdb.com/api/2.0/public/cards'
+    ).json()['data']
+    card_data = sorted(card_data, key=get_title)
+    collection.insert_many([
+        {
+            'name': name,
+            'packs': [printing['pack_code'] for printing in printings]
+        }
+        for name, printings in groupby(card_data, get_title)
+    ])
