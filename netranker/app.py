c15ce8520c90187c72814d3f377f6d3c9d2159bc
Daniel Strong <dstrong@glyx.co.uk>
2019-05-31 12:36:57 +0100

    Handle auth failures using werkzeug exceptions

--- netranker/app.py
+++ netranker/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
+from werkzeug.exceptions import Unauthorized, Forbidden, BadRequest
 
 import jwt
 
@@ -25,24 +26,24 @@ class ResultApi(Resource):
     def post(self):
         auth_header = request.headers.get('authorization', None)
         if auth_header is None:
-            return 'Forbidden', 403
+            raise Forbidden
 
         authorization, token = auth_header.split(' ')
         if authorization.lower() != 'bearer':
-            return 'Unauthorized', 401
+            raise Unauthorized
 
         try:
             claims = jwt.decode(
                 token, app.config['HMAC_KEY'], algorithms=['HS256']
             )
         except jwt.InvalidTokenError:
-            return 'Unauthorized', 401
+            raise Unauthorized
 
         winner = request.json.get('winner', None)
         if winner is None:
-            return 'Bad Request', 400
+            raise BadRequest
         if winner not in claims['cards']:
-            return 'Unauthorized', 401
+            raise Unauthorized
 
         return None, 204
 
