383f423bdeda51019a1b873e2e771d3020bd4912
Daniel Strong <dstrong@glyx.co.uk>
2019-06-06 19:40:31 +0100

    Add default sampler into settings

--- netranker/settings.py
+++ netranker/settings.py
@@ -1,8 +1,10 @@
 from secrets import token_hex
 
 import netranker.storage
+import netranker.samplers
 
 HMAC_KEY = token_hex(256)
 RESTFUL_JSON = {'ensure_ascii': False}
 CARD_STORAGE = netranker.storage.InMemoryCardStorage()
 RESULT_STORAGE = netranker.storage.InMemoryResultStorage()
+SAMPLER = netranker.samplers.SimpleRandom(CARD_STORAGE)
