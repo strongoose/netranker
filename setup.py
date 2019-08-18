974290140b1a430869ba06d67a6fb40ef26be557
Daniel Strong <dstrong@glyx.co.uk>
2019-08-18 12:56:02 +0100

    Bump version to allow submission to pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="netranker",
-    version="0.0.1",
+    version="0.0.2",
     author="Daniel Strong",
     author_email="dstrong@glyx.co.uk",
     description="Flask APIs for netranker",
