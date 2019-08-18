4b0d53a358791d9670982198b5e6e48248ff677c
Daniel Strong <dstrong@glyx.co.uk>
2019-08-18 13:11:21 +0100

    Fix drone publish step

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="netranker",
-    version="0.0.2",
+    version="0.0.3",
     author="Daniel Strong",
     author_email="dstrong@glyx.co.uk",
     description="Flask APIs for netranker",
