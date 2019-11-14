598075ed989ebdb7398750f1aa139adbe600f493
Daniel Strong <dstrong@glyx.co.uk>
2019-11-14 20:43:50 +0000

    Fix issue with test CARDS being mutated by insert in functional tests

--- test_utils.py
+++ test_utils.py
@@ -4,7 +4,10 @@ from test_data import CARDS
 
 def load_test_data(card_storage):
     for card in CARDS:
-        card_storage.insert(card)
+        # pymongo adds '_id' fields to documents it inserts; to prevent this
+        # mutating the top level CARDS list we make a copy each doc before
+        # inserting
+        card_storage.insert(card.copy())
 
 def random_card_names(size):
     return [card['name'] for card in sample(CARDS, size)]
